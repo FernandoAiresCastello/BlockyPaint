811c7d5785877a7ae0dbf4d2d06ecca18524a742
Fernando <fernandoairescastello@gmail.com>
2020-11-14 19:05:40 -0300

    First commit

---
+++ BlockyPaint/MainWindow.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace BlockyPaint
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.PalettePanel = new System.Windows.Forms.Panel();
+            this.CanvasPanel = new System.Windows.Forms.Panel();
+            this.BtnColor = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.StSize = new System.Windows.Forms.ToolStripTextBox();
+            this.BtnNew = new System.Windows.Forms.ToolStripButton();
+            this.BtnFill = new System.Windows.Forms.ToolStripButton();
+            this.BtnIncrease = new System.Windows.Forms.ToolStripButton();
+            this.BtnDecrease = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(97, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 525);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 501);
+            this.panel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 138F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.toolStrip1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PalettePanel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.CanvasPanel, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 501);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // toolStrip1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.toolStrip1, 2);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.BtnNew,
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripSeparator3,
+            this.BtnFill,
+            this.BtnIncrease,
+            this.BtnDecrease,
+            this.toolStripSeparator2,
+            this.toolStripLabel1,
+            this.BtnColor,
+            this.toolStripLabel2,
+            this.StSize});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 26);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // PalettePanel
+            // 
+            this.PalettePanel.BackColor = System.Drawing.Color.Black;
+            this.PalettePanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PalettePanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PalettePanel.Location = new System.Drawing.Point(3, 29);
+            this.PalettePanel.Name = "PalettePanel";
+            this.PalettePanel.Size = new System.Drawing.Size(132, 469);
+            this.PalettePanel.TabIndex = 1;
+            // 
+            // CanvasPanel
+            // 
+            this.CanvasPanel.BackColor = System.Drawing.SystemColors.Control;
+            this.CanvasPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.CanvasPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CanvasPanel.Location = new System.Drawing.Point(141, 29);
+            this.CanvasPanel.Name = "CanvasPanel";
+            this.CanvasPanel.Size = new System.Drawing.Size(656, 469);
+            this.CanvasPanel.TabIndex = 2;
+            // 
+            // BtnColor
+            // 
+            this.BtnColor.Margin = new System.Windows.Forms.Padding(1, 3, 1, 0);
+            this.BtnColor.Name = "BtnColor";
+            this.BtnColor.ReadOnly = true;
+            this.BtnColor.Size = new System.Drawing.Size(25, 23);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(42, 23);
+            this.toolStripLabel1.Text = "Color :";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 26);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Margin = new System.Windows.Forms.Padding(3, 1, 0, 2);
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(33, 23);
+            this.toolStripLabel2.Text = "Size :";
+            // 
+            // StSize
+            // 
+            this.StSize.Margin = new System.Windows.Forms.Padding(1, 3, 1, 0);
+            this.StSize.Name = "StSize";
+            this.StSize.ReadOnly = true;
+            this.StSize.Size = new System.Drawing.Size(40, 23);
+            // 
+            // BtnNew
+            // 
+            this.BtnNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnNew.Image = global::BlockyPaint.Properties.Resources.page_white;
+            this.BtnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnNew.Name = "BtnNew";
+            this.BtnNew.Size = new System.Drawing.Size(23, 23);
+            this.BtnNew.Text = "toolStripButton1";
+            this.BtnNew.Click += new System.EventHandler(this.BtnNew_Click);
+            // 
+            // BtnFill
+            // 
+            this.BtnFill.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnFill.Image = global::BlockyPaint.Properties.Resources.paintcan;
+            this.BtnFill.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnFill.Name = "BtnFill";
+            this.BtnFill.Size = new System.Drawing.Size(23, 23);
+            this.BtnFill.Text = "toolStripButton1";
+            this.BtnFill.Click += new System.EventHandler(this.BtnFill_Click);
+            // 
+            // BtnIncrease
+            // 
+            this.BtnIncrease.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnIncrease.Image = global::BlockyPaint.Properties.Resources.magnifier_zoom_in;
+            this.BtnIncrease.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnIncrease.Name = "BtnIncrease";
+            this.BtnIncrease.Size = new System.Drawing.Size(23, 23);
+            this.BtnIncrease.Text = "toolStripButton1";
+            this.BtnIncrease.Click += new System.EventHandler(this.BtnIncrease_Click);
+            // 
+            // BtnDecrease
+            // 
+            this.BtnDecrease.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.BtnDecrease.Image = global::BlockyPaint.Properties.Resources.magnifier_zoom_out;
+            this.BtnDecrease.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.BtnDecrease.Name = "BtnDecrease";
+            this.BtnDecrease.Size = new System.Drawing.Size(23, 23);
+            this.BtnDecrease.Text = "toolStripButton2";
+            this.BtnDecrease.Click += new System.EventHandler(this.BtnDecrease_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = global::BlockyPaint.Properties.Resources.folder;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 23);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = global::BlockyPaint.Properties.Resources.diskette;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 23);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 26);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 547);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Blocky Paint";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.Panel PalettePanel;
+        private System.Windows.Forms.Panel CanvasPanel;
+        private System.Windows.Forms.ToolStripButton BtnNew;
+        private System.Windows.Forms.ToolStripButton BtnFill;
+        private System.Windows.Forms.ToolStripButton BtnIncrease;
+        private System.Windows.Forms.ToolStripButton BtnDecrease;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripTextBox BtnColor;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripTextBox StSize;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+    }
+}
+
---
+++ BlockyPaint/MainWindow.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlockyPaint
+{
+    public partial class MainWindow : Form
+    {
+        private Bitmap Palette = (Bitmap)Image.FromFile("palette.png");
+        private Bitmap Canvas;
+        private Graphics CanvasGfx;
+        private SolidBrush MainColor;
+        private int BrushWidth = 30;
+        private int BrushHeight = 30;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateNewCanvas();
+            SetMainColor(Color.Black);
+            UpdateBrushSizeLabel();
+
+            PalettePanel.Paint += PalettePanel_Paint;
+            PalettePanel.Click += PalettePanel_Click;
+            CanvasPanel.Paint += CanvasPanel_Paint;
+            CanvasPanel.Click += CanvasPanel_Click;
+            CanvasPanel.MouseMove += CanvasPanel_MouseMove;
+        }
+
+        private void CreateNewCanvas()
+        {
+            Canvas = new Bitmap(CanvasPanel.ClientSize.Width, CanvasPanel.ClientSize.Height);
+            CanvasGfx = Graphics.FromImage(Canvas);
+            CanvasGfx.Clear(Color.White);
+            CanvasPanel.Invalidate();
+        }
+
+        private void SetMainColor(Color color)
+        {
+            MainColor = new SolidBrush(color);
+            BtnColor.BackColor = color;
+        }
+
+        private void PalettePanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Palette, 0, 0);            
+        }
+
+        private void PalettePanel_Click(object sender, EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+
+            try
+            {
+                SetMainColor(Palette.GetPixel(me.X, me.Y));
+            }
+            catch
+            {
+            }
+        }
+
+        private void CanvasPanel_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawImage(Canvas, 0, 0);
+        }
+
+        private void CanvasPanel_Click(object sender, EventArgs e)
+        {
+            CanvasClicked(e);
+        }
+
+        private void CanvasPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            CanvasClicked(e);
+        }
+
+        private void CanvasClicked(EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            if (me.Button == MouseButtons.Left)
+                PutBlock(e);
+            else if (me.Button == MouseButtons.Right)
+                GrabColor(e);
+        }
+
+        private void PutBlock(EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+            int x = me.X;
+            int y = me.Y;
+            Rectangle rect = new Rectangle(x, y, BrushWidth, BrushHeight);
+            CanvasGfx.FillRectangle(MainColor, rect);
+            CanvasPanel.Invalidate(rect);
+        }
+
+        private void GrabColor(EventArgs e)
+        {
+            MouseEventArgs me = (MouseEventArgs)e;
+
+            try
+            {
+                SetMainColor(Canvas.GetPixel(me.X, me.Y));
+            }
+            catch
+            {
+            }
+        }
+
+        private void BtnNew_Click(object sender, EventArgs e)
+        {
+            CreateNewCanvas();
+        }
+
+        private void BtnFill_Click(object sender, EventArgs e)
+        {
+            CanvasGfx.Clear(MainColor.Color);
+            CanvasPanel.Invalidate();
+        }
+
+        private void BtnIncrease_Click(object sender, EventArgs e)
+        {
+            BrushWidth += 10;
+            BrushHeight += 10;
+            UpdateBrushSizeLabel();
+        }
+
+        private void BtnDecrease_Click(object sender, EventArgs e)
+        {
+            if (BrushWidth > 10 && BrushHeight > 10)
+            {
+                BrushWidth -= 10;
+                BrushHeight -= 10;
+                UpdateBrushSizeLabel();
+            }
+        }
+
+        private void UpdateBrushSizeLabel()
+        {
+            if (BrushWidth == BrushHeight)
+                StSize.Text = BrushWidth.ToString();
+            else
+                StSize.Text = BrushWidth + "x" + BrushHeight;
+        }
+    }
+}
